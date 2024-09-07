c58dc2177809fe10391e4902473797646b1036cb
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-07 10:57:07 +0900

    :package: chore: 유저 Storage 생성 - 유저 Storage 모듈 추가

---
+++ storage/db-core-user/src/test/java/com/springcloud/eureka/dbcoreuser/DbCoreUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.eureka.dbcoreuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+class DbCoreUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
