06200d2142fa601141aaa4efb84aa322fe1eb73f
Skadi <74538745+SerenityZenDev@users.noreply.github.com>
2024-09-07 21:03:35 +0900

    ✨ feat: 유저 storage 모듈 생성 (#4)
    * :package: chore: 유저 Storage 생성
    - 유저 Storage 모듈 추가
    
    * :heavy_plus_sign: chore: 유저 Storage 생성
    - 루트 모듈에 유저 Storage 추가
    
    * :sparkles: feat: 유저 엔티티 생성
    
    * :sparkles: feat: 유저 리포지토리 생성
    
    * :white_check_mark: test: 유저 Jpa & Access 테스트
    
    * :art: style: gradle format 적용
    
    * :card_file_box: build: mysql -> postresql로 의존성 변경
    
    * :recycle: refactor: audit 필드 분리
    
    * :truck: chore: RoleType core 패키지 이동
    
    * :wrench: feat: h2 db config 추가
    
    * :white_check_mark: test: test 코드 패키지 이동 및 h2 local profile 적용 확인
    
    * :truck: chore: 패키지 이름 변경
    
    * :art: chore: format 적용
    
    * :art: refactor: 권한명 조정 및 format
    
    * :bug: fix: 테스트코드 프로필 미적용 수정

---
+++ storage/db-core-user/src/test/java/com/phoenix/logistics/storage/db/core/user/DbCoreUserApplicationTests.java
@@ -0,0 +1,15 @@
+package com.phoenix.logistics.storage.db.core.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+class DbCoreUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
