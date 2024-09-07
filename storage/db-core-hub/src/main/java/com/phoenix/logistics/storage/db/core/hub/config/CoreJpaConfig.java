d0fc3c20406c8b377885dd26005454ce78376b1c
Hyyena <sjtxm0320@gmail.com>
2024-09-07 21:21:01 +0900

    ✨ feat: db-core-hub 모듈 추가

---
+++ storage/db-core-hub/src/main/java/com/phoenix/logistics/storage/db/core/hub/config/CoreJpaConfig.java
@@ -0,0 +1,14 @@
+package com.phoenix.logistics.storage.db.core.hub.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EntityScan(basePackages = "com.phoenix.logistics.storage.db.core")
+@EnableJpaRepositories(basePackages = "com.phoenix.logistics.storage.db.core")
+class CoreJpaConfig {
+
+}
