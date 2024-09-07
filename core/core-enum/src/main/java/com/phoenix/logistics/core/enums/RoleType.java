c264f0b09fc4d51a70ad8711fca6beaada913a5b
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-07 17:47:01 +0900

    :truck: chore: RoleType core 패키지 이동

---
+++ core/core-enum/src/main/java/com/phoenix/logistics/core/enums/RoleType.java
@@ -0,0 +1,10 @@
+package com.phoenix.logistics.core.enums;
+
+public enum RoleType {
+    MASTER_ADMIN,
+    HUB_ADMIN,
+    DELIVERY_STAFF,   // 기존 HUB_DELIVERY_AGENT에서 변경
+    HUB_VENDOR,
+    MANUFACTURER      // 기존 HUB_MANUFACTUR에서 변경
+}
+
--- storage/db-core-user/src/main/java/com/springcloud/eureka/dbcoreuser/entity/RoleType.java
+++
@@ -1,7 +0,0 @@
-package com.springcloud.eureka.dbcoreuser.entity;
-
-public enum RoleType {
-
-    MASTER_ADMIN, HUB_ADMIN, HUB_DELIVERY_AGENT, HUB_VENDOR, HUB_MANUFACTUR
-
-}
