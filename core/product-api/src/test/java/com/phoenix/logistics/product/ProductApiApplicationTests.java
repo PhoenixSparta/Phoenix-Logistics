3baed623242fd65991a617c0a300d283008b133f
road3144 <fnfnfn258@gmail.com>
2024-09-07 22:15:31 +0900

    :sparkles: chore: Product API, Storage 모듏 추가

---
+++ core/product-api/src/test/java/com/phoenix/logistics/product/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.logistics.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
