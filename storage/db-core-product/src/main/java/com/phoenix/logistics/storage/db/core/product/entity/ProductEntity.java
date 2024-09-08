395e26a1f7ef675895f240dcb64b8f48f69b2034
TAEHWAN GIL <fnfnfn258@gmail.com>
2024-09-08 14:08:46 +0900

    ✨ feat: Product API, Storage 모듈 추가 (#6)
    * :sparkles: chore: Product API, Storage 모듏 추가
    
    * :sparkles: feat: Product(Domain), ProductEntity, ProductRepository
    
    * :art: style: modify with comments

---
+++ storage/db-core-product/src/main/java/com/phoenix/logistics/storage/db/core/product/entity/ProductEntity.java
@@ -0,0 +1,57 @@
+package com.phoenix.logistics.storage.db.core.product.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "p_products")
+public class ProductEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "product_uuid")
+    private UUID uuid;
+
+    @Column(name = "manufacturer_uuid", nullable = false)
+    private UUID manufacturerUuid;
+
+    @Column(name = "hub_uuid", nullable = false)
+    private UUID hubUuuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Boolean isDelete;
+
+    @Builder
+    public ProductEntity(UUID manufacturerUUID, UUID hubUUID, String name, Integer stock, Integer price,
+            Boolean isDelete) {
+        this.manufacturerUuid = manufacturerUUID;
+        this.hubUuuid = hubUUID;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.isDelete = isDelete;
+    }
+
+}
