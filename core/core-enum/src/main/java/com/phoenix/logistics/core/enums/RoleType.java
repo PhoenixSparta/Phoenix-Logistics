107d81f51fca6a9936a7a19bc52e7f9ca8fbb3c2
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-09 12:22:26 +0900

    :art: refactor: format 적용

--- core/core-enum/src/main/java/com/phoenix/logistics/core/enums/RoleType.java
+++ core/core-enum/src/main/java/com/phoenix/logistics/core/enums/RoleType.java
@@ -2,6 +2,7 @@ package com.phoenix.logistics.core.enums;
 
 public enum RoleType {
 
-    MASTER_ADMIN, HUB_MANAGER, DELIVERY_STAFF, VENDOR, MANUFACTURER, USER // 첫 회원가입 후 사용할 임시 권한
+    MASTER_ADMIN, HUB_MANAGER, DELIVERY_STAFF, VENDOR, MANUFACTURER, USER // 첫 회원가입 후 사용할
+                                                                          // 임시 권한
 
 }
