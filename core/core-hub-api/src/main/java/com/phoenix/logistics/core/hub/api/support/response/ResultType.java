d7774362e41f8a403a29fec0a9e8a1c3dc859833
Hyyena <sjtxm0320@gmail.com>
2024-09-10 11:57:11 +0900

    ✨ feat: 허브 API 모듈 및 베이스 코드 추가

---
+++ core/core-hub-api/src/main/java/com/phoenix/logistics/core/hub/api/support/response/ResultType.java
@@ -0,0 +1,7 @@
+package com.phoenix.logistics.core.hub.api.support.response;
+
+public enum ResultType {
+
+    SUCCESS, ERROR
+
+}
---
+++ core/core-hub-api/src/test/java/com/phoenix/logistics/CoreHubApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.phoenix.logistics;
+
+import org.junit.jupiter.api.Test;
+
+public class CoreHubApiApplicationTest extends ContextTest {
+
+    @Test
+    public void shouldBeLoadedContext() {
+        // it should be passed
+    }
+
+}
