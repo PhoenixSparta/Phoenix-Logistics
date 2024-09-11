a0ee5bb643a8e8d48f853e030be71b45cd2eca20
TAEHWAN GIL <fnfnfn258@gmail.com>
2024-09-11 10:25:44 +0900

    :art: refactor: 상품 도메인 모듈 분리 (#25)

--- core/core-product-domain/src/main/java/com/phoenix/logistics/core/product/domain/repository/ProductRepository.java
+++ core/core-product-domain/src/main/java/com/phoenix/logistics/core/product/domain/repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package com.phoenix.logistics.core.product.domain.repository;
 
-import org.springframework.data.repository.NoRepositoryBean;
-
 import com.phoenix.logistics.core.product.domain.model.Product;
 
-@NoRepositoryBean
 public interface ProductRepository {
 
     Product save(Product product);
