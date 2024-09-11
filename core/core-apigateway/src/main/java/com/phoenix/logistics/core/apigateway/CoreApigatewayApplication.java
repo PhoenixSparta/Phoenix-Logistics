bbd30af34659442a5b7956220345872589c0edc9
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-11 12:22:16 +0900

    :package: feat: API Gateway 모듈 생성

---
+++ core/core-apigateway/src/main/java/com/phoenix/logistics/core/apigateway/CoreApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix.logistics.core.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApigatewayApplication.class, args);
+    }
+
+}
---
+++ core/core-apigateway/src/test/java/com/phoenix/logistics/core/apigateway/CoreApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.logistics.core.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class CoreApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
