8f268592cf01bc1ecb829f5fea09616c1e002cae
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-12 11:31:45 +0900

    :package: feat: Redis 모듈 생성

---
+++ storage/db-core-redis/src/main/java/com/phoenix/logistics/db/core/redis/DbCoreRedisApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix.logistics.db.core.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCoreRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbCoreRedisApplication.class, args);
+    }
+
+}
---
+++ storage/db-core-redis/src/test/java/com/phoenix/logistics/db/core/redis/DbCoreRedisApplicationTests.java
@@ -0,0 +1,12 @@
+package com.phoenix.logistics.db.core.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class DbCoreRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
