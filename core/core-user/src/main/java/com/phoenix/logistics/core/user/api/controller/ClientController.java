dbd4385bde9d315ab5be7b1df4bf4c9c432d292c
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-17 17:09:27 +0900

    :recycle: refactor: feign 요청 인가 제외

--- core/core-user/src/main/java/com/phoenix/logistics/core/user/api/controller/ClientController.java
+++ core/core-user/src/main/java/com/phoenix/logistics/core/user/api/controller/ClientController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("feign/users")
 public class ClientController {
 
     private final UserService userService;
