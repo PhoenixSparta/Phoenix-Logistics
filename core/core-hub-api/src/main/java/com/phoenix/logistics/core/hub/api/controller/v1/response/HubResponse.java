1e792310ffba00783dc9da6aec4ad193676a39cc
Hyyena <sjtxm0320@gmail.com>
2024-09-19 02:21:52 +0900

    ✨ feat: 허브 검색 및 목록 조회 API 추가

--- core/core-hub-api/src/main/java/com/phoenix/logistics/core/hub/api/controller/v1/response/HubResponse.java
+++ core/core-hub-api/src/main/java/com/phoenix/logistics/core/hub/api/controller/v1/response/HubResponse.java
@@ -2,6 +2,7 @@ package com.phoenix.logistics.core.hub.api.controller.v1.response;
 
 import com.phoenix.logistics.core.hub.domain.HubResult;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record HubResponse(UUID hubUuid, int sequence, String name, String city, String fullAddress, double latitude,
@@ -11,5 +12,9 @@ public record HubResponse(UUID hubUuid, int sequence, String name, String city, 
         return new HubResponse(hubResult.hubUuid(), hubResult.sequence(), hubResult.name(), hubResult.city(),
                 hubResult.fullAddress(), hubResult.latitude(), hubResult.longitude(), hubResult.timestamp().createdAt(),
                 hubResult.timestamp().updatedAt());
+    }
+
+    public static List<HubResponse> of(List<HubResult> hubResults) {
+        return hubResults.stream().map(HubResponse::of).toList();
     }
 }
