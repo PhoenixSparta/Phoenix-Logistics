7de1037b14cca6c7da229928c0356c2013dd894e
Hyyena <sjtxm0320@gmail.com>
2024-09-19 11:16:32 +0900

    🐛 fix: 패키지 경로 수정

--- core/core-delivery-domain/src/main/java/com/phoenix/logistics/core/delivery/domain/Cursor.java
+++ core/core-delivery-domain/src/main/java/com/phoenix/logistics/core/delivery/domain/Cursor.java
@@ -1,4 +1,4 @@
-package com.phoenix.logistics.core.hub.domain;
+package com.phoenix.logistics.core.delivery.domain;
 
 public record Cursor(String query, String searchBy, String cursor, int limit, String sortKey, SortDirection sort) {
 }
