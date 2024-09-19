a0dbd7524e4ed90914779ba969741caa0de27d8a
Hyyena <sjtxm0320@gmail.com>
2024-09-19 18:16:42 +0900

    ✨ feat: 배송 조회 비즈니스 로직 추가

--- core/core-delivery-domain/src/main/java/com/phoenix/logistics/core/delivery/domain/DeliveryService.java
+++ core/core-delivery-domain/src/main/java/com/phoenix/logistics/core/delivery/domain/DeliveryService.java
@@ -1,5 +1,6 @@
 package com.phoenix.logistics.core.delivery.domain;
 
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,12 +8,19 @@ public class DeliveryService {
 
     private final DeliveryRegister deliveryRegister;
 
-    public DeliveryService(DeliveryRegister deliveryRegister) {
+    private final DeliveryReader deliveryReader;
+
+    public DeliveryService(DeliveryRegister deliveryRegister, DeliveryReader deliveryReader) {
         this.deliveryRegister = deliveryRegister;
+        this.deliveryReader = deliveryReader;
     }
 
     public DeliveryWithUuid register(Delivery delivery) {
         return deliveryRegister.register(delivery);
+    }
+
+    public DeliveryResult read(UUID deliveryUuid) {
+        return deliveryReader.read(deliveryUuid);
     }
 
 }
