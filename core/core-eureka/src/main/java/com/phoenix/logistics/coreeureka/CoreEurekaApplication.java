72ae00fa85f95e4dc6d51beda3d12b4043824d82
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-19 10:57:16 +0900

    :package: chore: Eureka 서버 설정 & 서비스 연결 확인

---
+++ core/core-eureka/src/main/java/com/phoenix/logistics/coreeureka/CoreEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.phoenix.logistics.coreeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class CoreEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreEurekaApplication.class, args);
+    }
+
+}
