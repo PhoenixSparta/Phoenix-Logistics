02a879b36e3fd0f55269bd924723e1a9b1c1b960
TAEHWAN GIL <fnfnfn258@gmail.com>
2024-09-19 18:55:58 +0900

    :bug: fix: pathVariable, response (#81)

---
+++ clients/client-delivery/src/main/java/com/phoenix/logistics/client/delivery/dto/DeliveryResponse.java
@@ -0,0 +1,8 @@
+package com.phoenix.logistics.client.delivery.dto;
+
+public record DeliveryResponse(String result,
+
+        DeliveryResponseData data
+
+) {
+}
---
+++ clients/client-delivery/src/main/java/com/phoenix/logistics/client/delivery/dto/DeliveryResponseData.java
@@ -0,0 +1,10 @@
+package com.phoenix.logistics.client.delivery.dto;
+
+import java.util.UUID;
+
+public record DeliveryResponseData(
+
+        UUID deliveryUuid
+
+) {
+}
