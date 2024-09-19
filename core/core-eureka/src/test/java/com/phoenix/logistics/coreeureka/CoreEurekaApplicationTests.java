6e545737a205aa7e50fa7dbfbe9f4ca8a0eb060b
Skadi <74538745+SerenityZenDev@users.noreply.github.com>
2024-09-19 11:02:29 +0900

    :package: chore: Eureka 서버 설정 & 서비스 연결 확인 (#64)

---
+++ core/core-eureka/src/test/java/com/phoenix/logistics/coreeureka/CoreEurekaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.phoenix.logistics.coreeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class CoreEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
