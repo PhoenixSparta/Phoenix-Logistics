7d927d2692ba96197663ba15a37a90040a406fad
Hyyena <sjtxm0320@gmail.com>
2024-09-20 01:09:36 +0900

    ✨ feat: 주문 요청 시 수령인 정보 추가 및 배송 로그 UUID 응답 스펙 추가

--- clients/client-delivery/src/main/java/com/phoenix/logistics/client/delivery/DeliveryClientAdapter.java
+++ clients/client-delivery/src/main/java/com/phoenix/logistics/client/delivery/DeliveryClientAdapter.java
@@ -1,14 +1,12 @@
 package com.phoenix.logistics.client.delivery;
 
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.stereotype.Component;
-
 import com.phoenix.logistics.client.delivery.dto.CreateDeliveryRequest;
+import com.phoenix.logistics.client.delivery.dto.DeliveryResponseData;
 import com.phoenix.logistics.core.product.domain.client.DeliveryClient;
+import com.phoenix.logistics.core.product.domain.model.DeliveryUuidWithLogUuid;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class DeliveryClientAdapter implements DeliveryClient {
@@ -21,13 +19,13 @@ public class DeliveryClientAdapter implements DeliveryClient {
     }
 
     @Override
-    public UUID createDelivery(UUID manufacturerUuid, UUID vendorUuid, UUID orderUuid, UUID sourceHubUuid,
-            UUID destinationHubUuid) {
+    public DeliveryUuidWithLogUuid createDelivery(UUID manufacturerUuid, UUID vendorUuid, UUID orderUuid,
+            UUID sourceHubUuid, UUID destinationHubUuid, String fullAddress, String recipientName,
+            String recipientSlackId) {
         CreateDeliveryRequest createDeliveryRequest = new CreateDeliveryRequest(manufacturerUuid, vendorUuid, orderUuid,
-                sourceHubUuid, destinationHubUuid);
-        UUID deliveryUuid = feignDeliveryClient.createDelivery(createDeliveryRequest).data().deliveryUuid();
-
-        return deliveryUuid;
+                sourceHubUuid, destinationHubUuid, fullAddress, recipientName, recipientSlackId);
+        DeliveryResponseData deliveryResponseData = feignDeliveryClient.createDelivery(createDeliveryRequest).data();
+        return new DeliveryUuidWithLogUuid(deliveryResponseData.deliveryUuid(), deliveryResponseData.deliveryLogUuid());
     }
 
 }
--- core/core-order-api/src/main/java/com/phoenix/logistics/core/order/api/controller/dto/CreateOrderRequest.java
+++ core/core-order-api/src/main/java/com/phoenix/logistics/core/order/api/controller/dto/CreateOrderRequest.java
@@ -1,15 +1,20 @@
 package com.phoenix.logistics.core.order.api.controller.dto;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
-
-import jakarta.validation.constraints.NotNull;
 
 public record CreateOrderRequest(
 
         @NotNull UUID manufacturerUuid,
 
         @NotNull UUID vendorUuid,
+
+        @NotNull String fullAddress,
+
+        @NotNull String recipientName,
+
+        @NotNull String recipientSlackId,
 
         @NotNull List<ProductOrderDto> productOrderDtoList
 
