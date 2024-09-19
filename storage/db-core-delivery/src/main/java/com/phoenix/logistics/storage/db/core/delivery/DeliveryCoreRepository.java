afa0a35d5ce6afec5439adf6b81cee8b5b9ebc96
Hyyena <sjtxm0320@gmail.com>
2024-09-19 18:16:04 +0900

    ✨ feat: 배송 조회 레포지토리 코드 추가

--- storage/db-core-delivery/src/main/java/com/phoenix/logistics/storage/db/core/delivery/DeliveryCoreRepository.java
+++ storage/db-core-delivery/src/main/java/com/phoenix/logistics/storage/db/core/delivery/DeliveryCoreRepository.java
@@ -2,7 +2,9 @@ package com.phoenix.logistics.storage.db.core.delivery;
 
 import com.phoenix.logistics.core.delivery.domain.Delivery;
 import com.phoenix.logistics.core.delivery.domain.DeliveryRepository;
+import com.phoenix.logistics.core.delivery.domain.DeliveryResult;
 import com.phoenix.logistics.core.delivery.domain.DeliveryWithUuid;
+import java.util.UUID;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,6 +19,11 @@ public class DeliveryCoreRepository implements DeliveryRepository {
     @Override
     public DeliveryWithUuid add(Delivery delivery) {
         return deliveryJpaRepository.save(DeliveryEntity.of(delivery)).toDeliveryWithUuid();
+    }
+
+    @Override
+    public DeliveryResult read(UUID deliveryUuid) {
+        return deliveryJpaRepository.findByUuid(deliveryUuid).orElseThrow().toDeliveryResult();
     }
 
 }
